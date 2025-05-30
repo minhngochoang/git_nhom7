7704673606cc11b415a78ee89e55d62ccd9cfa92
ADMIN <ADMIN@MINHNGOCHOANG>
2025-05-31 00:18:31 +0700

    Module13_hanghoa

--- QuanLySSCenter/module13.Designer.cs
+++ QuanLySSCenter/module13.Designer.cs
@@ -30,37 +30,37 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(module13));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
             this.guna2AnimateWindow1 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
-            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.btnThongke = new Guna.UI2.WinForms.Guna2Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dtpDenngay = new System.Windows.Forms.DateTimePicker();
+            this.dtpTungay = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
             this.guna2DataGridView1 = new Guna.UI2.WinForms.Guna2DataGridView();
             this.btnexit = new Guna.UI2.WinForms.Guna2Button();
             ((System.ComponentModel.ISupportInitialize)(this.guna2DataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // guna2Button1
+            // btnThongke
             // 
-            this.guna2Button1.BorderRadius = 18;
-            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.guna2Button1.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button1.Image")));
-            this.guna2Button1.ImageSize = new System.Drawing.Size(17, 17);
-            this.guna2Button1.Location = new System.Drawing.Point(1120, 20);
-            this.guna2Button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(187, 56);
-            this.guna2Button1.TabIndex = 24;
-            this.guna2Button1.Text = "Thống kê";
+            this.btnThongke.BorderRadius = 18;
+            this.btnThongke.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnThongke.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnThongke.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnThongke.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnThongke.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnThongke.ForeColor = System.Drawing.Color.White;
+            this.btnThongke.Image = ((System.Drawing.Image)(resources.GetObject("btnThongke.Image")));
+            this.btnThongke.ImageSize = new System.Drawing.Size(17, 17);
+            this.btnThongke.Location = new System.Drawing.Point(1005, 16);
+            this.btnThongke.Name = "btnThongke";
+            this.btnThongke.Size = new System.Drawing.Size(166, 45);
+            this.btnThongke.TabIndex = 24;
+            this.btnThongke.Text = "Thống kê";
+            this.btnThongke.Click += new System.EventHandler(this.btnThongke_Click);
             // 
             // label1
             // 
@@ -68,27 +68,25 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label1.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.label1.Location = new System.Drawing.Point(666, 31);
+            this.label1.Location = new System.Drawing.Point(565, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 38);
+            this.label1.Size = new System.Drawing.Size(113, 31);
             this.label1.TabIndex = 23;
             this.label1.Text = "Đến ngày";
             // 
-            // dateTimePicker1
+            // dtpDenngay
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(832, 41);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(256, 26);
-            this.dateTimePicker1.TabIndex = 22;
+            this.dtpDenngay.Location = new System.Drawing.Point(719, 28);
+            this.dtpDenngay.Name = "dtpDenngay";
+            this.dtpDenngay.Size = new System.Drawing.Size(228, 22);
+            this.dtpDenngay.TabIndex = 22;
             // 
-            // dateTimePicker2
+            // dtpTungay
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(366, 41);
-            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(255, 26);
-            this.dateTimePicker2.TabIndex = 21;
+            this.dtpTungay.Location = new System.Drawing.Point(265, 30);
+            this.dtpTungay.Name = "dtpTungay";
+            this.dtpTungay.Size = new System.Drawing.Size(227, 22);
+            this.dtpTungay.TabIndex = 21;
             // 
             // label2
             // 
@@ -96,42 +94,41 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label2.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.label2.Location = new System.Drawing.Point(155, 29);
+            this.label2.Location = new System.Drawing.Point(117, 23);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 38);
+            this.label2.Size = new System.Drawing.Size(98, 31);
             this.label2.TabIndex = 20;
             this.label2.Text = "Từ ngày";
             // 
             // guna2DataGridView1
             // 
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
-            this.guna2DataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.guna2DataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.White;
+            this.guna2DataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle10;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            dataGridViewCellStyle11.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.guna2DataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle11;
             this.guna2DataGridView1.ColumnHeadersHeight = 4;
             this.guna2DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.guna2DataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            dataGridViewCellStyle12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.guna2DataGridView1.DefaultCellStyle = dataGridViewCellStyle12;
             this.guna2DataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            this.guna2DataGridView1.Location = new System.Drawing.Point(56, 111);
-            this.guna2DataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.guna2DataGridView1.Location = new System.Drawing.Point(50, 89);
             this.guna2DataGridView1.Name = "guna2DataGridView1";
             this.guna2DataGridView1.RowHeadersVisible = false;
             this.guna2DataGridView1.RowHeadersWidth = 51;
             this.guna2DataGridView1.RowTemplate.Height = 24;
-            this.guna2DataGridView1.Size = new System.Drawing.Size(1241, 656);
+            this.guna2DataGridView1.Size = new System.Drawing.Size(1103, 525);
             this.guna2DataGridView1.TabIndex = 25;
             this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
             this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.Font = null;
@@ -166,29 +163,28 @@
             this.btnexit.ForeColor = System.Drawing.Color.White;
             this.btnexit.Image = ((System.Drawing.Image)(resources.GetObject("btnexit.Image")));
             this.btnexit.ImageSize = new System.Drawing.Size(70, 70);
-            this.btnexit.Location = new System.Drawing.Point(-8, 11);
-            this.btnexit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnexit.Location = new System.Drawing.Point(-6, 16);
             this.btnexit.Name = "btnexit";
-            this.btnexit.Size = new System.Drawing.Size(106, 56);
+            this.btnexit.Size = new System.Drawing.Size(94, 45);
             this.btnexit.TabIndex = 36;
             this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
             // 
             // module13
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
-            this.ClientSize = new System.Drawing.Size(1385, 874);
+            this.ClientSize = new System.Drawing.Size(1231, 699);
             this.Controls.Add(this.btnexit);
             this.Controls.Add(this.guna2DataGridView1);
-            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.btnThongke);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dtpDenngay);
+            this.Controls.Add(this.dtpTungay);
             this.Controls.Add(this.label2);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "module13";
-            this.Text = "Chi tiết doanh thu theo hóa đơn bán và mặt hàng";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Doanh thu của các mặt hàng";
             ((System.ComponentModel.ISupportInitialize)(this.guna2DataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -197,10 +193,10 @@
 
         #endregion
         private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow1;
-        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button btnThongke;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dtpDenngay;
+        private System.Windows.Forms.DateTimePicker dtpTungay;
         private System.Windows.Forms.Label label2;
         private Guna.UI2.WinForms.Guna2DataGridView guna2DataGridView1;
         private Guna.UI2.WinForms.Guna2Button btnexit;
--- QuanLySSCenter/module13.cs
+++ QuanLySSCenter/module13.cs
@@ -7,24 +7,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TP_BVSK
 {
     public partial class module13 : Form
     {
+        string connectionString = "Data Source=MINHNGOCHOANG;Initial Catalog=BTLNhom7;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+
         public module13()
         {
             InitializeComponent();
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void btnThongke_Click(object sender, EventArgs e)
         {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                DateTime tuNgay = dtpTungay.Value;
+                DateTime denNgay = dtpDenngay.Value;
+                decimal tongDoanhThu = 0;
+
+                // Lấy dữ liệu chi tiết
+                DataTable dt = new DataTable();
+                using (SqlCommand cmd = new SqlCommand("spDoanhThuTheoThoiGian", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = 120; // thời gian tối đa là 120 giây
+
+                    cmd.Parameters.AddWithValue("@TuNgay", tuNgay);
+                    cmd.Parameters.AddWithValue("@DenNgay", denNgay);
+
+                    SqlParameter pTongDT = new SqlParameter("@TongDoanhThu", SqlDbType.Money)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+                    cmd.Parameters.Add(pTongDT);
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.Fill(dt);
+
+
+                    tongDoanhThu = (pTongDT.Value != DBNull.Value) ? Convert.ToDecimal(pTongDT.Value) : 0;
+                }
+
+                // Nếu không có dữ liệu, thông báo và thoát
+                if (dt.Rows.Count == 0)
+                {
+                    MessageBox.Show("Không có dữ liệu trong khoảng thời gian đã chọn.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    guna2DataGridView1.DataSource = null;
+                    return;
+                }
+                // Thêm dòng tổng doanh thu
+                DataRow rowTong = dt.NewRow();
+                rowTong["Doanh Thu"] = tongDoanhThu;
+                dt.Rows.Add(rowTong);
+
+                // Gán vào DataGridView
+                guna2DataGridView1.DataSource = dt;
+
+                // Tùy chỉnh tên cột
+                guna2DataGridView1.Columns["MAHANG"].HeaderText = "Mã hàng";
+                guna2DataGridView1.Columns["TENHANG"].HeaderText = "Tên hàng";
+                guna2DataGridView1.Columns["Doanh Thu"].HeaderText = "Doanh thu (VNĐ)";
+
+                // Hiển thị tổng doanh thu
+                guna2DataGridView1.Text = "Tổng doanh thu: " + tongDoanhThu.ToString("N0") + " VNĐ";
+                foreach (DataGridViewColumn col in guna2DataGridView1.Columns)
+                {
+                    col.SortMode = DataGridViewColumnSortMode.Automatic;
+                }
+
+                // Vô hiệu hóa sắp xếp cột "Tên nhân viên"
+                guna2DataGridView1.Columns["TENHANG"].SortMode = DataGridViewColumnSortMode.NotSortable;
+
+
+            }
 
         }
-
         private void btnexit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
     }
 }
